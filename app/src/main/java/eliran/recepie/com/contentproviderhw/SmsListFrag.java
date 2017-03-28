544ce4bd52b9d1d20a0632260513b1047b189ed8
eliranmoshe <kaza.moshe@gmail.com>
2017-03-28 23:07:53 +0200

    work with email BODY=TEXT(String)

--- app/src/main/java/eliran/recepie/com/contentproviderhw/SmsListFrag.java
+++ app/src/main/java/eliran/recepie/com/contentproviderhw/SmsListFrag.java
@@ -12,12 +12,17 @@ import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -38,16 +43,38 @@ RecyclerView recyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_sms_list, container, false);
+        final View view=inflater.inflate(R.layout.fragment_sms_list, container, false);
         IntentFilter intentFilter=new IntentFilter("com.recepiesearch.eliran.DONE!");
         LocalBroadcastManager.getInstance(getActivity()).registerReceiver(new SmsListener(),intentFilter);
         recyclerView= (RecyclerView) view.findViewById(R.id.SmsListRV);
-        Intent intent=new Intent(getActivity(),GetSmsListService.class);
+        final Intent intent=new Intent(getActivity(),GetSmsListService.class);
         getActivity().startService(intent);
-        getActivity().findViewById(R.id.ExportBtn).setOnClickListener(new View.OnClickListener() {
+        Button ExportBtn= (Button) view.findViewById(R.id.ExportBtn);
+        ExportBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO shared oll AllSms to text file
+                ArrayList<String> StringArray=new ArrayList<String>();
+                //StringBuilder stringBuilder=new StringBuilder();
+                Gson gson=new Gson();
+                for (int i=0;i<AllSms.size();i++)
+                {
+                   StringArray.add(gson.toJson(AllSms.get(i)));
+                    //stringBuilder.append(AllSms.get(i));
+                }
+                Log.d("dvfds","fsdfsd");
+                StringBuffer result = new StringBuffer();
+                for (int i = 0; i < StringArray.size(); i++) {
+                    result.append( StringArray.get(i) );
+                    //result.append( optional separator );
+                }
+                String mynewstring = result.toString();
+               // String s=stringBuilder.toString();
+               // TinyDB tinyDB=new TinyDB(getActivity());
+               // tinyDB.putString("jsonlist",stringBuilder);
+                Intent intent1=new Intent(Intent.ACTION_SEND);
+                intent1.setType("message/rfc822");
+                intent1.putExtra(Intent.EXTRA_TEXT,mynewstring);
+                getActivity().startActivity(intent1);
             }
         });
 
@@ -58,7 +85,7 @@ RecyclerView recyclerView;
 
         return view;
     }
- 
+
 
     public class SmsListener extends BroadcastReceiver{
 
